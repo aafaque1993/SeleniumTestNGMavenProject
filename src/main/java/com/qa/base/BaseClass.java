2122d753eefd9f33120889635291cf9081102ea3
aafaque1993 <aafaque1993@gmail.com>
2021-12-24 03:12:17 +0530

    pom.xml and testng.xml integrated for parallel run

--- src/main/java/com/qa/base/BaseClass.java
+++ src/main/java/com/qa/base/BaseClass.java
@@ -5,6 +5,8 @@ import java.util.Properties;
 import org.openqa.selenium.WebDriver;
 import com.qa.commonutils.Constants;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class BaseClass {
 
 	public static WebDriver driver;
@@ -19,6 +21,8 @@ public class BaseClass {
 			FileInputStream ip = new FileInputStream(Constants.PROP_FILE_PATH);
 			prop.load(ip);
 			System.out.println("Base Class Created");
+			WebDriverManager.chromedriver().setup();
+			
 
 		}
 
---
+++ src/test/java/com/qa/testlogic/TestCase3.java
@@ -0,0 +1,47 @@
+package com.qa.testlogic;
+
+import java.util.List;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+public class TestCase3 {
+	
+	
+	
+	
+	
+	@FindBy(xpath = "//sample")
+	private WebElement element;
+
+	WebDriver driver = new ChromeDriver();
+
+	@Test
+	public void startCase1() {
+		System.out.println("Case 1");
+
+	}
+
+	@Test
+	public void startCase2() {
+		System.out.println("Case 2");
+
+	}
+
+	@Test
+	public void startCase3() {
+		System.out.println("Case 3");
+		
+		driver.quit();
+		
+	}
+
+}
